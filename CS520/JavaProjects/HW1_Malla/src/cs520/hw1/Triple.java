d2d42869d4d510867d02836b194f19e9d879e514
parijatmalla <parijat.malla@gmail.com>
2015-03-21 03:34:40 +0100

    Updated variable name to meet variable naming convention

--- CS520/JavaProjects/HW1_Malla/src/cs520/hw1/Triple.java
+++ CS520/JavaProjects/HW1_Malla/src/cs520/hw1/Triple.java
@@ -1,40 +1,39 @@
 package cs520.hw1;
 
-/**
- * @author Parijat Malla
- * 
- */
-
 /*
+ * @author Parijat Malla
  * Program to generate Pythagorean triple between numbers 1 to 100.
  */
+
 public class Triple {
 
 	public static void main(String[] args) {
 
 		double z = 0;
-		double sumofsquares=0;
+		double sumOfSquares = 0;
 
 		int i = 1;
 
-		System.out.println("Count"+"\tx\ty\tz");
+		System.out.println("Count" + "\tx\ty\tz");
 		for (int x = 1; x <= 100; x++) {
 
-			//loop for y starting from x+1 so that duplicate combination of pythagorean triples are avoided
+			// loop for y starting from x+1 so that duplicate combination of
+			// pythagorean triples are avoided
 			for (int y = x + 1; y <= 100; y++) {
-				
-				//calculate sum of squares of x and y
-				sumofsquares=x * x + y * y;
 
-				//compute square root for sum of squares of x and y
-				
-				z = Math.sqrt(sumofsquares);
+				// calculate sum of squares of x and y
+				sumOfSquares = x * x + y * y;
 
-				//check if sum of the squares of x and y is equal to square of integer z
-				if (sumofsquares == (int) z*(int)z) {
+				// compute square root for sum of squares of x and y
 
-					System.out.println(i + ")" +"\t"+ x + "\t " + y
-							+ " \t" + (int) z);
+				z = Math.sqrt(sumOfSquares);
+
+				// check if sum of the squares of x and y is equal to square of
+				// integer z
+				if (sumOfSquares == (int) z * (int) z) {
+
+					System.out.println(i + ")" + "\t" + x + "\t " + y + " \t"
+							+ (int) z);
 					i++;
 
 				}
