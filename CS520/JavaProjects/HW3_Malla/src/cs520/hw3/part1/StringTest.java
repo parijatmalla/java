f677c3c4da4f3fda21431fe86de8456891da9ebb
parijatmalla <parijat.malla@gmail.com>
2015-04-06 03:41:21 +0200

    Added negative number exception handling and zero length input exception

--- CS520/JavaProjects/HW3_Malla/src/cs520/hw3/part1/StringTest.java
+++ CS520/JavaProjects/HW3_Malla/src/cs520/hw3/part1/StringTest.java
@@ -29,112 +29,98 @@ public class StringTest {
 		String courseID, courseName;
 		String tuition;
 
-		// flag to check for completion
-		// boolean done = false;
-
-		// while (!done) {
-
 		input = JOptionPane
 				.showInputDialog("Please enter data using the format CourseID, CourseName, Tuition:");
 
-		// when user presses cancel
-		if (input == null) {
-			System.out.println("Program has been cancelled");
-			System.exit(1);
+		try {
+			// trim the input to remove leading and trailing white spaces
+			input = input.trim();
+			System.out.println(input);
 
+			// find index of first occurrence of comma
+			indexOfFirstComma = input.indexOf(",");
+
+			System.out.print("First Comma Position:" + indexOfFirstComma
+					+ " , ");
+			// find out course ID from input string
+			courseID = input.substring(0, indexOfFirstComma);
+			courseID = courseID.trim();
+
+			// handle exception when course id is not entered
+			if (courseID.length() == 0)
+				throw new ZeroLengthInput("Course ID has not been entered.");
+			System.out.print("Course Id:" + courseID + " , ");
+			System.out.println("Length :" + courseID.length());
+
+			// find index of second comma
+			indexOfSecondComma = input.indexOf(",", indexOfFirstComma + 1);
+
+			System.out.print("Second Comma Position:" + indexOfSecondComma
+					+ " , ");
+
+			// find out course Name from input string
+			courseName = input.substring(indexOfFirstComma + 1,
+					indexOfSecondComma);
+			courseName = courseName.trim();
+
+			// handle exception when course name is not entered
+			if (courseName.length() == 0)
+				throw new ZeroLengthInput("Course Name has not been entered.");
+
+			System.out.print("Course Name:" + courseName + " , ");
+			System.out.println("Length:" + courseName.length());
+			// }
+			// done = true;
+
+			// try {
+			// find out tuition from input string
+
+			tuition = input.substring(indexOfSecondComma + 1, input.length());
+			tuition = tuition.trim();
+			// handle exception if tuition fee is not entered
+			if (tuition.length() == 0)
+				throw new ZeroLengthInput("Tuition  has not been entered.");
+
+			// handle exception when tuition fee provided is negative
+			if (Integer.parseInt(tuition) < 0)
+				throw new NegativeNumberException(
+						"Tuition Fee is negative number.Please enter correct tuition fee");
+			System.out.print("Regular Tuition: $" + tuition + " , ");
+
+			// trim spaces from discount
+			int discount = Integer.parseInt(tuition.trim());
+			discount = (int) (discount * 0.75);
+
+			// display discounted tuition fee which is 75% of tuition
+			System.out.println("Discounted Tuition: $" + discount);
+			// done = true;
 		}
 
-		else {
+		// handle exception when tuition fee provided is not a number
+		catch (NumberFormatException e1) {
+			System.out.println(e1+" Invalid Tuition fee provided. " );
+			System.exit(0);
+		}
+		// Null pointer exception when user cancels and no data is feed
+		catch (NullPointerException e) {
 
-			// handle exception when only course ID is provided
-			try {
-				// trim the input to remove leading and trailing white spaces
-				input = input.trim();
-
-				// find index of first occurrence of comma
-				indexOfFirstComma = input.indexOf(",");
-
-				// handle exception when user presses Ok with no data input
-				if (input.length() <= 0)
-					throw new Exception(
-							"No data has been entered. Program terminated");
-				else {
-					// display trimmed input
-					System.out.println( input);
-				}
-
-				// handle exception when no comma separated data is input
-				if (indexOfFirstComma == -1)
-					throw new Exception(
-							"No Course Name and tuition fee is not provided.");
-				else {
-					System.out.print("First Comma Position:"
-							+ indexOfFirstComma + " , ");
-					// find out course ID from input string
-					courseID = input.substring(0, indexOfFirstComma);
-					if (courseID.length() <= 0)
-						throw new Exception("Enter valid Course ID");
-					else {
-						System.out.print("Course Id:" + courseID + " , ");
-						System.out.println("Length :" + courseID.length());
-					}
-				}
-				// done = true;
-
-				// handle exception when only courseID and course name is
-				// provided
-				// but tuition is not provided
-				// try {
-				// find index of second comma
-				indexOfSecondComma = input.indexOf(",", indexOfFirstComma + 1);
-
-				// handle exception when only course id is provided
-				if (indexOfSecondComma == -1)
-					throw new Exception(
-							"No course name and tuition is provided. Program terminated");
-				else
-					System.out.print("Second Comma Position:"
-							+ indexOfSecondComma + " , ");
-
-				// find out course Name from input string
-				courseName = input.substring(indexOfFirstComma + 1,
-						indexOfSecondComma);
-
-				if (courseName.length() <= 0)
-					throw new Exception(
-							"Enter valid Course Name. Please try again!");
-				else {
-					System.out.print("Course Name:" + courseName + " , ");
-					System.out.println("Length:" + courseName.length());
-					// }
-					// done = true;
-
-				}
-
-				// handle exception when tuition fee provided is not a number
-				// try {
-				// find out tuition from input string
-
-				tuition = input.substring(indexOfSecondComma + 1,
-						input.length());
-
-				System.out.print("Regular Tuition: $" + tuition + " , ");
-
-				// trim spaces from discount
-				int discount = Integer.parseInt(tuition.trim());
-				discount = (int) (discount * 0.75);
-
-				// display discounted tuition fee which is 75% of tuition
-				System.out.println("Discounted Tuition: $" + discount);
-				// done = true;
-			} catch (Exception thrownObject) {
-
-				System.out.println(thrownObject.getMessage());
-				System.exit(0);
-
-			}
-			
+			System.out.println("The program has been cancelled." + e);
+			System.exit(0);
 
 		}
+		// handle exception when only course ID is provided
+		catch (StringIndexOutOfBoundsException e) {
+			System.out
+					.println("Incomplete data provided.Please provide CourseID, CourseName and tuition fee."
+							+ e);
+			System.exit(0);
+		} catch (NegativeNumberException e) {
+			System.out.println(e);
+		}
+
+		catch (ZeroLengthInput e) {
+			System.out.println(e);
+		}
+
 	}
 }
