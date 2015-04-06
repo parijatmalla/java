bc3c612a89917fdc013aa12ecf634321a2f78130
parijatmalla <parijat.malla@gmail.com>
2015-04-06 04:07:48 +0200

    Updated to remove case and change processinputdata to private

--- CS520/JavaProjects/HW3_Malla/src/cs520/hw3/part2/Test.java
+++ CS520/JavaProjects/HW3_Malla/src/cs520/hw3/part2/Test.java
@@ -17,54 +17,40 @@ import java.util.StringTokenizer;
 public class Test {
 
 	public Test() {
-		// TODO Auto-generated constructor stub
+
 	}
 
-	public static void processInputData(String st) {
+	private void processInputData(String st) {
 		StringTokenizer st1 = new StringTokenizer(st, ",");
-		String token;
-		int i = 1;
-		Student currentStudent = new Student();
 
-		while (st1.hasMoreTokens()) {
-			token = st1.nextToken();
+		// check if all the homework scores and name are provided in the input
+		// file
+		// if all the fields are not provided, it exits.
+		if (st1.countTokens() < 7) {
+			System.out
+					.println("All the required data fields are not provided in the input file");
+			System.exit(0);
+		} else {
+			Student currentStudent = new Student();
 
-			switch (i) {
-			case 1:
+			while (st1.hasMoreTokens()) {
 
-				currentStudent.setName(token);
-				i++;
-				break;
-			case 2:
-				currentStudent.setHomework1(Integer.parseInt(token));
-				i++;
-				break;
-			case 3:
-				currentStudent.setHomework2(Integer.parseInt(token));
-				i++;
-				break;
-			case 4:
-				currentStudent.setHomework3(Integer.parseInt(token));
-				i++;
-				break;
-			case 5:
-				currentStudent.setHomework4(Integer.parseInt(token));
-				i++;
-				break;
-			case 6:
-				currentStudent.setHomework5(Integer.parseInt(token));
-				i++;
-				break;
-			case 7:
-				currentStudent.setHomework6(Integer.parseInt(token));
-				i++;
-				break;
+				currentStudent.setName(st1.nextToken());
+				currentStudent.setHomework1(Integer.parseInt(st1.nextToken()));
+				currentStudent.setHomework2(Integer.parseInt(st1.nextToken()));
+				currentStudent.setHomework3(Integer.parseInt(st1.nextToken()));
+
+				currentStudent.setHomework4(Integer.parseInt(st1.nextToken()));
+
+				currentStudent.setHomework5(Integer.parseInt(st1.nextToken()));
+
+				currentStudent.setHomework6(Integer.parseInt(st1.nextToken()));
+
 			}
 			// System.out.println("token-i"+token+","+i);
+			System.out.printf(currentStudent.toString());
 
 		}
-
-		System.out.printf(currentStudent.toString());
 	}
 
 	/**
@@ -72,6 +58,9 @@ public class Test {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
+
+		Test t1 = new Test();
+
 		// read input from file data.txt
 		BufferedReader inputStream = null;
 		String line = "";
@@ -79,19 +68,18 @@ public class Test {
 		try {
 			inputStream = new BufferedReader(new FileReader("data.txt"));
 			// System.out.println("buffered");
-			
-		}
-		catch(FileNotFoundException e){
-			System.out.println(e+"File not found in provided location");
+
+		} catch (FileNotFoundException e) {
+			System.out.println(e + "File not found in provided location");
 			System.exit(0);
 		}
 
-		try{
+		try {
 			line = inputStream.readLine();
 			while (line != null) {
 
 				// System.out.println(line);
-				processInputData(line);
+				t1.processInputData(line);
 				line = inputStream.readLine();
 				// line=inputStream.readLine();
 				// System.out.println("in readline");
@@ -109,5 +97,6 @@ public class Test {
 		finally {
 			inputStream.close();
 		}
+
 	}
 }
