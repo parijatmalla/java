fe3303ac3472037ae85cdd58e2dd18602ddaea36
parijatmalla <parijat.malla@gmail.com>
2015-04-14 03:25:04 +0200

    cleaned up the code

--- CS520/JavaProjects/HW4_Malla2/src/cs520/hw4/Legos1.java
+++ CS520/JavaProjects/HW4_Malla2/src/cs520/hw4/Legos1.java
@@ -19,65 +19,36 @@ public class Legos1 extends JFrame {
 		this.startY = 300;
 		this.legoWidth = 50;
 		this.legoHeight = 20;
-		// by setting the baselength the height of the lego can be changed
+		// by setting the baselength the height of the triangle can be changed
 		this.baseLength = 10;
 	}
 
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
-		int X, Y;
-		X = startX;
-		Y = startY;
-
 		// loop to draw rectangles in vertical order
-		for (int j = 1; j <= baseLength; j++) {
-			boolean colorRed = true;
+		for (int j = 0; j < baseLength; j++) {
 			// to draw rectangles with alternate red-blue color in any
 			// horizontal row
 			// for first row there would be all the rectangles, but as we go up
 			// in the legos the number of rectangles in a row decreases, so the
-			// maximum value of i is set baseLength-1-j so that with increase in
+			// maximum value of i is set baseLength-j so that with increase in
 			// height of lego, the number of rectangles in each row decreases
-			for (int i = 1; i <= baseLength + 1 - j; i++) {
-
-				if (colorRed == true) {
-
-					g.setColor(Color.RED);
-					// to change the color of rectangle to blue
-					colorRed = false;
-				} else {
-					g.setColor(Color.BLUE);
-					// if color of rectangle has been changed to blue, for next
-					// rectangle it needs to be changed to red so colorRed is
-					// set to true
-					colorRed = true;
-				}
-
+			g.setColor(Color.RED);
+			for (int i = 0; i < baseLength - j; i++) {
 				// Draw Filled rectangle and square
-				//using arcwidth and archeight as 2
-				g.fillRoundRect(X, Y, legoWidth, legoHeight, 2, 2);
-				X = X + legoWidth;// move X to new X position
-
+				// using arcwidth and archeight as 2
+				g.fillRoundRect((startX + (legoWidth * i) + j * (legoWidth / 2)),
+						startY - (legoHeight * j), legoWidth, legoHeight, 2, 2);
+				g.setColor((g.getColor() == Color.RED) ? Color.BLUE : Color.RED);
 			}
-			// move Y to new position to draw rectangles in next row
-			Y = Y - legoHeight;
-
-			// move X to new position to start drawing the rectangles from the
-			// center of first rectangle in previous row
-			X = startX + (j) * legoWidth / 2;
-			// startX=X;
 		}
 	}
 
 	public static void main(String[] args) {
 		Legos1 lg = new Legos1();
-
 		lg.setSize(550, 325);
 		lg.setVisible(true);
-
 		lg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 	}
-
 }
